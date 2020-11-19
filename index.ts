b6a23226ec82e05fa91203c7226b7deaa1cc8e67
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2020-11-19 22:59:45 +0100

    Added basic Nginx deployment.

--- index.ts
+++ index.ts
@@ -1,14 +1,32 @@
 import * as k8s from "@pulumi/kubernetes";
 
 const appLabels = { app: "nginx" };
+
+const service = new k8s.core.v1.Service("nginx", {
+    spec: {
+        type: 'LoadBalancer',
+        ports: [{ port: 8080, protocol: 'TCP', targetPort: 80 }],
+        selector: appLabels
+    }
+});
+
 const deployment = new k8s.apps.v1.Deployment("nginx", {
     spec: {
         selector: { matchLabels: appLabels },
         replicas: 1,
         template: {
             metadata: { labels: appLabels },
-            spec: { containers: [{ name: "nginx", image: "nginx" }] }
+            spec: { 
+                containers: [
+                    { 
+                        name: "nginx", 
+                        image: "nginx:1.19.4-alpine", 
+                        ports: [{containerPort: 80}]
+                    }
+                ] 
+            }
         }
     }
 });
+
 export const name = deployment.metadata.name;
